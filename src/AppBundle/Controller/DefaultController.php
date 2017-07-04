b6bf67c65eacd589b7508c57959a43ad16d0bb62
ManuelAgustinLucero <apattyeffect@gmail.com>
2017-07-04 17:35:51 -0300

    v3.0 mapa listo

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -5,7 +5,8 @@ namespace AppBundle\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
-
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 class DefaultController extends Controller
 {
     /**
@@ -29,4 +30,75 @@ class DefaultController extends Controller
         // replace this example code with whatever you need
         return $this->render(':default:mapa.html.twig');
     }
+
+
+
+
+
+
+    /**
+     * Displays a form to edit an existing Students entity.
+     *
+     * @Route("/carga", name="carga_mapaa")
+     * @Method({"GET", "POST"})
+     */
+    public function cargamarcadores(Request $request)
+    {
+        $idproblematica = $request->request->get('id');
+        
+        $em = $this->getDoctrine()->getManager();
+        $db = $em->getConnection();
+        $query ="SELECT problematica.id,problematica.ciudad,problematica.latitud,problematica.longitud,problematica.descripcion,tipo_problematica.nombre,tipo_problematica.marcador FROM problematica INNER JOIN tipo_problematica ON problematica.tipoproblematica_id=tipo_problematica.id";
+        
+        $stmt = $db->prepare($query);
+        $params = array();
+        $stmt->execute($params);
+        $problematicas=$stmt->fetchAll();
+        
+        foreach ($problematicas as $problematica){
+            $result[]=$problematica;
+            
+
+        }
+        return new JsonResponse($result);
+
+
+    }
+
+    /**
+     * Displays a form to edit an existing Students entity.
+     *
+     * @Route("/remplaza", name="remplaza_mapaa")
+     * @Method({"GET", "POST"})
+     */
+    public function remplaza(Request $request)
+    {
+        $idproblematica = $request->request->get('option');
+
+        $em = $this->getDoctrine()->getManager();
+        $db = $em->getConnection();
+        if ($idproblematica==7){
+            $query ="SELECT problematica.id,problematica.ciudad,problematica.latitud,problematica.longitud,problematica.descripcion,tipo_problematica.nombre,tipo_problematica.marcador FROM problematica INNER JOIN tipo_problematica ON problematica.tipoproblematica_id=tipo_problematica.id";
+
+        }else{
+            $query ="SELECT problematica.id,problematica.ciudad,problematica.latitud,problematica.longitud,problematica.descripcion,tipo_problematica.nombre,tipo_problematica.marcador FROM problematica INNER JOIN tipo_problematica ON problematica.tipoproblematica_id=tipo_problematica.id WHERE  tipo_problematica.id='$idproblematica'";
+
+        }
+
+        $stmt = $db->prepare($query);
+        $params = array();
+        $stmt->execute($params);
+        $problematicas=$stmt->fetchAll();
+
+        foreach ($problematicas as $problematica){
+            $result[]=$problematica;
+
+
+        }
+
+        return new JsonResponse($result);
+
+
+    }
+
 }
